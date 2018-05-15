bec47c9e9183484d21470713c4ecd20c3622d826
ronin <XXX@W.com>
2018-05-15 16:00:01 +0800

    first commit

---
+++ src/page/aboutUs.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        关于我们
+        <div class="test">
+            力于建设行业信息化及应用软件的研制开发，是集软件开发、系统集成与产品服务为
+            <div class="ff">
+asdasd
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+@red: pink;
+.test {
+    color: @red;
+    .ff {
+        color: yellow;
+    }
+}
+</style>
