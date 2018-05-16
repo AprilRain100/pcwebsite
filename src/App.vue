316d48b8fc8bb23aed45d373470e15b54a4b8d84
ronin <XXX@W.com>
2018-05-16 09:47:29 +0800

    轮播组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+        <router-link :to="'/'">首页</router-link>
+                 <router-link :to="'/aboutUs'">关于我们</router-link>
+        <router-link :to="'/contactUs'">联系我们</router-link>
+        <router-link :to="'/newsCenter'">新闻资讯</router-link>
+        <router-link :to="'/employers'">企业服务</router-link>
+        <router-link :to="'/onlineMessage'">在线留言</router-link>
     <router-view/>
   </div>
 </template>
--- src/page/home.vue
+++ src/page/home.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        <!--<router-link :to="'/aboutUs'">关于我们</router-link>-->
-        <!--<router-link :to="'/contactUs'">联系我们</router-link>-->
-        <!--<router-link :to="'/newsCenter'">新闻资讯</router-link>-->
-        <!--<router-link :to="'/employers'">企业服务</router-link>-->
-        <!--<router-link :to="'/onlineMessage'">在线留言</router-link>-->
-        <!--<router-link :to="'/'">首页</router-link>-->
-
+      home
+      <slideShow></slideShow>
     </div>
 </template>
+<script>
+import slideShow from '../components/slideshow'
+export default {
+  data () {
+    return {
+      height: '', // 轮播图组件的高
+      imgList: [] // 轮播图组件的图片
+    }
+  },
+  computed: {
+  },
+  components: {
+    slideShow
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
